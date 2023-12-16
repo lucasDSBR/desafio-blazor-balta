e6f3902ff564a3a8f7647788f4c813808991a0d8
FernandoVoigt <fe.voigt17@hotmail.com>
2023-12-16 18:32:53 -0300

    Criacao, edicao e exclusao de itens

--- BlazorBalta/Services/IbgeService.cs
+++ BlazorBalta/Services/IbgeService.cs
@@ -41,7 +41,7 @@ namespace BlazorBalta.Services
 
         public async Task<List<Ibge>> FindAll()
         {
-            var IbgeList = await _context.Ibge.OrderByDescending(i => i.Id).ToListAsync();
+            var IbgeList = await _context.Ibge.OrderBy(i => i.Id).Take(30).ToListAsync();
             return IbgeList;
         }
         public List<Ibge> BuscarFiltro(Ibge ibge)
@@ -58,10 +58,11 @@ namespace BlazorBalta.Services
         }
         public async Task<List<string>> FindEstados()
         {
-            List<string> EstadosList = null;
+            List<string> EstadosList = new();
             EstadosList = await _context.Ibge
                 .Select(e => e.State)
                 .Distinct()
+                .OrderBy(e => e)
                 .ToListAsync();
             return EstadosList;
 
@@ -72,6 +73,7 @@ namespace BlazorBalta.Services
             CidadesList = await _context.Ibge
                 .Select(c => c.City)
                 .Distinct()
+                .OrderBy(c => c)
                 .ToListAsync();
             return CidadesList;
         }
